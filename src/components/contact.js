d563541767e6a14584d568c3c7363fead6a37a2d
Arun Seehra <seehra.a@gmail.com>
2018-11-05 11:50:12 -0600

    Use semantic html

--- src/components/contact.js
+++ src/components/contact.js
@@ -5,10 +5,10 @@ import './contact.css';
 // The Contact component goes here.  It should be the default export.
 export default function Contact(props) {
   return (
-    <div className="contact">
+    <section className="contact">
       <img className="contact-photo" src={props.photo} alt={props.name} />
       <h2 className="contact-name">{props.name}</h2>
-      <div className="contact-address">{props.address}</div>
-    </div>
+      <address className="contact-address">{props.address}</address>
+    </section>
   );
 }
