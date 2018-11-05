a59e299e20ff144e7cdf36156aa6288a10470e53
Arun Seehra <seehra.a@gmail.com>
2018-11-05 11:47:35 -0600

    Our solution for address book challenge

--- src/components/contact.js
+++ src/components/contact.js
@@ -3,3 +3,12 @@ import React from 'react';
 import './contact.css';
 
 // The Contact component goes here.  It should be the default export.
+export default function Contact(props) {
+  return (
+    <div className="contact">
+      <img className="contact-photo" src={props.photo} alt={props.name} />
+      <h2 className="contact-name">{props.name}</h2>
+      <div className="contact-address">{props.address}</div>
+    </div>
+  );
+}
